13aa81a079046cc6baca6bbbf940ed41b4ed9400
AnastasiiaDem <85587393+AnastasiiaDem@users.noreply.github.com>
2023-07-05 15:55:35 -0400

    Updated

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -12,10 +12,31 @@ export class MainComponent {
   //     $("#pointer").css({ left: e.pageX - 120, top: e.pageY - 120 });
   // }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let home = (document.querySelector('.home') as HTMLElement).clientHeight;
+    let about = (document.querySelector('#about') as HTMLElement).clientHeight;
+    let projects = (document.querySelector('#projects') as HTMLElement).clientHeight;
+    let contact = (document.querySelector('#contact') as HTMLElement).clientHeight;
+
+    let element = document.querySelector('.floating-indicator') as HTMLElement;
+
+    if (window.pageYOffset < home - 400) {
+      element.style.transform = 'translateX(0%)';
+    } else if (window.pageYOffset > home - 100 && window.pageYOffset < home + about - 500) {
+      element.style.transform = 'translateX(100%)';
+    } else if (window.pageYOffset > projects + 550 && window.pageYOffset < home + about + projects - 550) {
+      element.style.transform = 'translateX(200%)';
+    } else if (window.pageYOffset > home + about + projects - 10) {
+      element.style.transform = 'translateX(300%)';
+    }
+  }
+
   x: number = 0;
 
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
 
   translate(v: number) {
@@ -24,5 +45,6 @@ export class MainComponent {
 
   navigateTo(page: string) {
     this.router.navigate(['/' + page]);
+    window.scrollTo(0, 0);
   }
 }
--- src/app/taskery/taskery.component.ts
+++ src/app/taskery/taskery.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {animate} from '@angular/animations';
 
 @Component({
   selector: 'taskery',
@@ -7,4 +9,32 @@ import {Component} from '@angular/core';
 })
 export class TaskeryComponent {
 
+  degrees = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+
+    let scrollPercent = scrollTop / (docHeight - winHeight);
+    let scrollPercentRounded = Math.round(scrollPercent * 100);
+    this.degrees = scrollPercent * 360;
+  }
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  navigateTo(page: string) {
+    this.router.navigate(['/' + page]);
+    window.scrollTo(0, 1150);
+    // document.querySelector('#project')?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  }
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
 }
